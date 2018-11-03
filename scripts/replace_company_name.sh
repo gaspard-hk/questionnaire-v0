ba9898ec4ac178643a664f3b8f7a92b4ab2557e9
Wong, Pak Yin <pakyin.wong@link.cuhk.edu.hk>
2018-11-04 02:00:37 +0800

    scripts updated

--- scripts/replace_company_name.sh
+++ scripts/replace_company_name.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/sh
+#to remove illegal byte sequence
+export LANG=C
 working_dir=$1
 target=$2
 replacement=$3
 cd $working_dir
-find "$working_dir" -type f -exec sed -i "s/$target/$replacement/g" {} +
+echo MAC
+find "$working_dir" -type f -exec sed -i '' -e "s/$target/$replacement/g" {} +
+echo Linux
+echo find "$working_dir" -type f -exec sed -i "s/$target/$replacement/g" {} +
 cd -
